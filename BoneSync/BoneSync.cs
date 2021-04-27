f7e9c112aca27f3339e89da85c39a847a0a7dfc9
LoVals <LoVals@users.noreply.github.com>
2021-04-27 09:14:31 +0100

    Bonesync structure
    Preparing system to accept fdp to XML conversion code

--- BoneSync/BoneSync.cs
+++ BoneSync/BoneSync.cs
@@ -3,38 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Drawing;
+using System.Data;
 
 namespace BoneSync
 {
     class BoneSync
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string TestFile = "Blank";
-            bool PathSet = false;
 
+            string TestFile = "Blank";
+            bool SetPath = false;
+            string rootPath = @"D:\Programs\Static\Dark_Souls_Mods\SoundSouls\SoundBones";      //Replace this with Current Directory + \Soundbones as the script resides in the same folder as pre build sync
+            string path = Directory.GetCurrentDirectory();
             Console.WriteLine(" _________________________");
             Console.WriteLine("¦                         ¦");
             Console.WriteLine("¦       Bonesync 1.0      ¦");
             Console.WriteLine("¦_________________________¦");
             Console.WriteLine("");
+            Console.WriteLine("The current directory is {0}", path);
             Console.WriteLine("Preparing to syncronize...");
-            if (PathSet = false)
+       
+            if (SetPath = false)
             {
-                Console.WriteLine("Filepath Not Found: ");
-                Console.WriteLine("Please, select the SoundSouls Filepath ");
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadLine();
-                //TODO - ADD A FUCKING FILE SELECTION IF PATH NOT SET
+                BoneSync.ChangeFolder();
             }
-            Console.WriteLine("Would you like to select a filepath for the ");
+            
             Console.ReadLine();
             Console.WriteLine("You Selected " + TestFile);
+            BoneSync.XMLMake(rootPath);
             Console.ReadLine();
 
-            //NEED TO WRITE THE SYNC ALGORITHM - NO CLUE HOW THE FUCK TO DO THAT YET
+            //NEED TO WRITE THE SYNC ALGORITHM - XML MERGE will likely be the way
 
 
+
+        }
+        public static void XMLMake(string rootPath)                                             //Will copy over the fdp files as xml to merge them subsequently
+        {
+                                                                                                //should change this if the user decides to select another directory
+            Console.WriteLine("XML conversion is starting");
+            Console.WriteLine("Fetching SoundBones...");
+            var files = Directory.GetFiles(rootPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void ChangeFolder()
+        {
+            //Code goes here
         }
     }
 }
