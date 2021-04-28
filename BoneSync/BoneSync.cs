31928bd546da34adb79008814d7c94b746748815
LoVals <LoVals@users.noreply.github.com>
2021-04-28 07:02:40 +0100

    Copy Files in XML
    Copies Soundbones in xml folder

--- BoneSync/BoneSync.cs
+++ BoneSync/BoneSync.cs
@@ -26,14 +26,15 @@ namespace BoneSync
             Console.WriteLine("");
             Console.WriteLine("The current directory is {0}", path);
             Console.WriteLine("Preparing to syncronize...");
-       
+
             if (SetPath = false)
             {
                 BoneSync.ChangeFolder();
             }
-            
+
             Console.ReadLine();
-            Console.WriteLine("You Selected " + TestFile);
+            //Console.WriteLine("You Selected " + TestFile);
+            BoneSync.WipeOldFiles();
             BoneSync.XMLMake(rootPath);
             Console.ReadLine();
 
@@ -42,23 +43,71 @@ namespace BoneSync
 
 
         }
-        public static void XMLMake(string rootPath)                                             //Will copy over the fdp files as xml to merge them subsequently
+        public static void XMLMake(string rootPath)
+        //Will copy over the fdp files as xml to merge them subsequently
         {
-                                                                                                //should change this if the user decides to select another directory
+
+            string sourceDir = @"D:\Programs\Static\Dark_Souls_Mods\SoundSouls\SoundBones";
+            string backupDir = @"D:\Programs\Static\Dark_Souls_Mods\SoundSouls\XMLs";
+            string fileName = @"D:\Programs\Static\Dark_Souls_Mods\SoundSouls\XMLs";
+
+
+            //should change this if the user decides to select another directory
             Console.WriteLine("XML conversion is starting");
             Console.WriteLine("Fetching SoundBones...");
             var files = Directory.GetFiles(rootPath, "*", SearchOption.TopDirectoryOnly);
+
+
             foreach (string file in files)
             {
                 Console.WriteLine(file);
             }
 
-            Console.ReadLine();
+            try
+            {
+                string[] BoneList = Directory.GetFiles(sourceDir, "*.fdp");
+                foreach (string f in BoneList)
+                {
+                    // Remove path from the file name.
+                    string fName = f.Substring(sourceDir.Length + 1);
+
+                    try
+                    {
+                        // Will not overwrite if the destination file already exists.
+                        File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName));
+                        //File.Move((Path.Combine(backupDir, fName), Path.Combine(backupDir, fName);
+                    }
+
+                    // Catch exception if the file was already copied.
+                    catch (IOException copyError)
+                    {
+                        Console.WriteLine(copyError.Message);
+                    }
+                
+
+                }
+            }
+
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                Console.WriteLine(dirNotFound.Message);
+            }
         }
 
         public static void ChangeFolder()
         {
             //Code goes here
+        }
+        public static void WipeOldFiles()
+        {
+            //Wipes any leftover file in the XML dir
+
+
+            string[] filePaths = Directory.GetFiles(@"D:\Programs\Static\Dark_Souls_Mods\SoundSouls\XMLs");
+            foreach (string filePath in filePaths)
+            {
+                File.Delete(filePath);
+            }
         }
     }
 }
