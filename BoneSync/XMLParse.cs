f89996aaa9489155999c1de1af147b2d8f9cbf77
LoVals <lorenzovalsassina@ied.edu>
2021-05-06 16:35:06 +0100

    Update XMLParse.cs
    Added parsing of XML files specific lines to be put on a list

--- BoneSync/XMLParse.cs
+++ BoneSync/XMLParse.cs
@@ -15,48 +15,75 @@ namespace BoneSync
 
         public static void ParseXML()
         {
-            //bool FileMatch = true;
+
             XDocument xmlA = XDocument.Load(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\TEST_A.xml");
-            IEnumerable<XElement> xElements = xmlA.Descendants("guid");
             XDocument xmlB = XDocument.Load(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\TEST_B.xml");
-            IEnumerable<XElement> xElementsB = xmlB.Descendants("guid");
             Console.Clear();
-            //XMLParse.CheckIfSame(@"D:\Programs\Static\Dark_Souls_Mods\SoundSouls\XMLs\SoundSoulsXML\TEST_A.xml", @"D:\Programs\Static\Dark_Souls_Mods\SoundSouls\XMLs\SoundSoulsXML\TEST_B.xml");
             Console.WriteLine("Parsing Files...");
-            //search for missing GUID - this is to add any event to that is muissing on the specific child
-            //might need to get the name of the child somehow: Possibly referencing it in FNC?
-
-            //Need to isolate searches to the following folders: sounddeffolder; eventcategory
-
-            foreach (XElement element in xElements)
-            {
-                Console.WriteLine("GuID in File A: " + element.Value);
-            }
-
-            foreach (XElement element in xElementsB)
-            {
-                Console.WriteLine("GuID in File B: " + element.Value);
-            }
-            XMLParse.DiffMatch();
+            XMLParse.DiffMatch(xmlA, xmlB);
             Console.ReadLine();
         }
 
-        public static void DiffMatch()
+        public static void DiffMatch(XDocument ModifiedProject, XDocument CachedProject)
         {
 
-            //Testing the Google DiffPatch API with this scrit
+            //Testing the Google DiffPatch API with this script
             //need to figure out if I can diff a whole file and output the result into a patch to apply to a specific bone
             //out of all the things I tried this is the most promising - DO NOT CAN THIS BUT EXPAND
+            //https://github.com/google/diff-match-patch for more info
 
-            diff_match_patch dmp = new diff_match_patch();
-            List<Diff> diff = dmp.diff_main("Hello World.", "Hello World.");
-            // Result: [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
-            dmp.diff_cleanupSemantic(diff);
-            // Result: [(-1, "Hello"), (1, "Goodbye"), (0, " World.")]
-            for (int i = 0; i < diff.Count; i++)
+            //XML comparison Setup for Main Projects: VARIABLES
+            //----------------------------------------------------------------------------
+            IEnumerable<XElement> SoundDefModifications = ModifiedProject.Descendants("sounddeffolder");
+            IEnumerable<XElement> EventGroupModifications = ModifiedProject.Descendants("eventgroup");
+            IEnumerable<XElement> SoundDefCache = CachedProject.Descendants("sounddeffolder");
+            IEnumerable<XElement> EventGroupCache = CachedProject.Descendants("eventgroup");
+            List<string> ModificationsList = new List<string>();
+            List<string> CacheList = new List<string>();
+            //----------------------------------------------------------------------------
+
+            //Reading values and adding them to a list
+            //----------------------------------------------------------------------------
+            foreach (XElement element in SoundDefModifications)
             {
-                Console.WriteLine(diff[i]);
+                Console.WriteLine("Detected Sound Def: " + element.Value);
+                ModificationsList.Add(element.Value);
+                Console.WriteLine();
             }
+
+            foreach (XElement element in EventGroupModifications)
+            {
+                Console.WriteLine("Detected Event: " + element.Value);
+                ModificationsList.Add(element.Value);
+                Console.WriteLine();
+            }
+
+            foreach (XElement element in SoundDefCache)
+            {
+                Console.WriteLine("Caching Sound Def from Cache: " + element.Value);
+                CacheList.Add(element.Value);
+                Console.WriteLine();
+            }
+
+            foreach (XElement element in EventGroupCache)
+            {
+                Console.WriteLine("Caching Event from Cache: " + element.Value);
+                CacheList.Add(element.Value);
+                Console.WriteLine();
+            }
+
+            diff_match_patch XmlDocs = new diff_match_patch();
+            List<Diff> diff = XmlDocs.diff_main("", "");
+            XmlDocs.patch_make("TextA","TextB");
+            //diff_match_patch dmp = new diff_match_patch();
+            //List<Diff> diff = dmp.diff_main("Hello World.", "Hello World.");
+            // Result: [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
+            //dmp.diff_cleanupSemantic(diff);
+            // Result: [(-1, "Hello"), (1, "Goodbye"), (0, " World.")]
+            //for (int i = 0; i < diff.Count; i++)
+           // {
+             //   Console.WriteLine(diff[i]);
+            //}
 
         }
     }
