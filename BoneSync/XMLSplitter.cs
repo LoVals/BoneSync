73ffb38b0a6803fd661c22191cd9949e2b33505d
LoVals <lorenzovalsassina@ied.edu>
2021-05-24 16:39:27 +0100

    Finishing XML Splitter Protocol

--- BoneSync/XMLSplitter.cs
+++ BoneSync/XMLSplitter.cs
@@ -21,55 +21,95 @@ namespace BoneSync
             string BackupFileLocation = @"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\SoundSouls.xml";
             File.Copy(SoundSoulsXML, BackupFileLocation);
             XDocument SkeletonFile = XDocument.Load(BackupFileLocation);
+
+            //----------------------------------------------------------------------------------------------------------------------------------------
+            //EVENTCATEGORYSPLIT
+            //----------------------------------------------------------------------------------------------------------------------------------------
             
             var EventCategoryNugget = SkeletonFile.Descendants("eventcategory").Select(d => new XDocument(new XElement("eventcategory", d)));
             int EvCounter = 0;
-            do
+            EventNuggetStart:
+            foreach (var TargetNugget in EventCategoryNugget)
             {
-                try
-                {
-                    foreach (var TargetNugget in EventCategoryNugget)
-                    {
-
-                        EvCounter = EvCounter + 1;
-                        Console.WriteLine("Nugget for Event Category 1");
-                        TargetNugget.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\Nuggets\EventCategoryNugget_0" + EvCounter + ".xml");
-                        TargetNugget.Remove(); //When parent is deleted, shit happens
-                        EventCategoryNugget = SkeletonFile.Descendants("eventcategory").Select(d => new XDocument(new XElement("eventcategory", d)));
-                    }
-                }
-                catch (System.InvalidOperationException); //ALL THIS NEEDS REWORK
-                                                          // IDEA - SXPORT THE 1300 files but cull the ones too small. They belong to children which can be considered useless data
+                EvCounter = EvCounter + 1;
+                TargetNugget.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\Nuggets\EventCategoryNugget_0" + EvCounter + ".xml");
+                break;
+            }
+            XElement DeletingCache = XElement.Load(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\SoundSouls.xml");
+            XElement DeleteME = DeletingCache.Element("eventcategory");
+            DeleteME.Remove();
+            DeletingCache.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\SoundSouls.xml");
+            //Overwriting the file is the key to get only what you need
+            SkeletonFile = XDocument.Load(BackupFileLocation);
+            EventCategoryNugget = SkeletonFile.Descendants("eventcategory").Select(d => new XDocument(new XElement("eventcategory", d)));
+            if (EvCounter != 5)
+            {
+                goto EventNuggetStart;
             }
 
-            while ();
-
-                //----------------------------------------------------------------------------------------------------------------------------------------------------------------
-                var SoundDefFolderNugget = SkeletonFile.Descendants("sounddeffolder").Select(d => new XDocument(new XElement("sounddeffolder", d)));
-                int SDFCounter = 0;
+            //----------------------------------------------------------------------------------------------------------------------------------------
+            //SOUNDDEFFOLDER
+            //----------------------------------------------------------------------------------------------------------------------------------------
+            
+            var SoundDefFolderNugget = SkeletonFile.Descendants("sounddeffolder").Select(d => new XDocument(new XElement("sounddeffolder", d)));
+            int SDFCounter = 0;
             foreach (var TargetNugget in SoundDefFolderNugget)
             {
                 SDFCounter = SDFCounter + 1;
                 TargetNugget.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\Nuggets\SoundDefFolderNugget_0" + SDFCounter + ".xml");
-                TargetNugget.Remove();
+                break;
             }
+            DeletingCache = XElement.Load(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\SoundSouls.xml");
+            DeleteME = DeletingCache.Element("sounddeffolder");
+            DeleteME.Remove();
+            DeletingCache.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\SoundSouls.xml");
+            //Overwriting the file is the key to get only what you need
+            SkeletonFile = XDocument.Load(BackupFileLocation);
+            EventCategoryNugget = SkeletonFile.Descendants("sounddeffolder").Select(d => new XDocument(new XElement("sounddeffolder", d)));
+            if (EvCounter != 5)
+            {
+                goto EventNuggetStart;
+            }
+
+            //----------------------------------------------------------------------------------------------------------------------------------------
+            //EVENTGROUP
+            //----------------------------------------------------------------------------------------------------------------------------------------
+
             var EventGroupNugget = SkeletonFile.Descendants("eventgroup").Select(d => new XDocument(new XElement("eventgroup", d)));
             int EGCounter = 0;
             foreach (var TargetNugget in EventGroupNugget)
             {
                 EGCounter = EGCounter + 1;
                 TargetNugget.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\Nuggets\EventGroupNugget_0" + EGCounter + ".xml");
-                TargetNugget.Remove(); TargetNugget.Remove();
+                break;
             }
+            DeletingCache = XElement.Load(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\SoundSouls.xml");
+            DeleteME = DeletingCache.Element("eventgroup");
+            DeleteME.Remove();
+            DeletingCache.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\SoundSouls.xml");
+
+            //----------------------------------------------------------------------------------------------------------------------------------------
+            //SOUNDBANK
+            //----------------------------------------------------------------------------------------------------------------------------------------
+
             var SoundbankNugget = SkeletonFile.Descendants("soundbank").Select(d => new XDocument(new XElement("soundbank", d)));
             int SBCounter = 0;
             foreach (var TargetNugget in SoundbankNugget)
             {
                 SBCounter = SBCounter + 1;
                 TargetNugget.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\Nuggets\SoundbankNugget_0" + SBCounter + ".xml");
-                TargetNugget.Remove();
+                break;
             }
+            DeletingCache = XElement.Load(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\SoundSouls.xml");
+            DeleteME = DeletingCache.Element("soundbank");
+            DeleteME.Remove();
+            DeletingCache.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\SoundSouls.xml");
         }
+
+        //----------------------------------------------------------------------------------------------------------------------------------------
+        //This is likely very inefficient - I am sure there are better ways to do this shit
+        //----------------------------------------------------------------------------------------------------------------------------------------
+
         public static void WipeOldNuggets()
         {
             string[] filePaths = Directory.GetFiles(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\PatchData\Nuggets\");
