e21bf5a0e372f0c6420e574f7d3a1c75a0693782
LoVals <lorenzovalsassina@ied.edu>
2021-06-03 15:45:19 +0100

    finished main rewriter

--- BoneSync/V2.0/BoneSync_02/BoneSync_02/GenerateChild.cs
+++ BoneSync/V2.0/BoneSync_02/BoneSync_02/GenerateChild.cs
@@ -17,6 +17,7 @@ namespace BoneSync_02
     {
         public static void Execute()
         {
+            Console.ForegroundColor = ConsoleColor.White;
             //What needs to be done:
 
             //>>    Detect what has changed
@@ -24,43 +25,136 @@ namespace BoneSync_02
 
             //------------------------------------------------------------------------------
             // LOAD PARENT FILE INTO MEMORY
-            var XParent = XElement.Load(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\V2.0\TestFiles\XML\Parent.xml");
             var ParentFile = XDocument.Load(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\V2.0\TestFiles\XML\Parent.xml");
             string BoneName = "Child1";
 
-            //-------------------------------------------------------------------------------
-            //                             Event Category
-            //-------------------------------------------------------------------------------
+            //-------------------------------------------------------------------------------//
+            //                             Event Category                                    //
+            //-------------------------------------------------------------------------------//
 
+            // THEORY - EVENTCATEGORIES NEED TO  BE IGNORED - they hold no definition of where their children resides. That could be defined in the EventGroup?
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Checking Event Category");
             var EventCategoryFolderAll = ParentFile.Descendants("eventcategory");
-            var MusicCategory = EventCategoryFolderAll.Descendants("name");
-            foreach (var ChildElement in MusicCategory)
+            int Shortcutvalue = 0;
+            foreach (var ChildElement in EventCategoryFolderAll)
             {
+
                 string TestMatch = ChildElement.Descendants("name").FirstOrDefault().Value;
+                switch (Shortcutvalue)
+                {
+                    case 0:
+                        goto _Music;
+
+                    case 1:
+                        goto _Ghost;
+
+                    case 2:
+                        goto _SE;
+
+                    case 3:
+                        goto _Voice;
+
+                    case 4:
+                        goto _Default;
+                }
+
+
+                _Music:
                 if (IsMusic(TestMatch) == true)
                 {
                     Console.WriteLine("music folder found");
-                    break;
+                    Shortcutvalue = 1;
                 }
-            }
-                //-------------------------------------------------------------------------------
-                //                             Sound Def Folder
-                //-------------------------------------------------------------------------------
 
-                var SounddefFolderAll = ParentFile.Descendants("sounddeffolder");
+                _Ghost:
+                if (IsGhost(TestMatch) == true)
+                {
+                    Console.WriteLine("Ghost folder found");
+                    Shortcutvalue = 2;
+                }
+
+                _SE:
+                if (IsSE(TestMatch) == true)
+                {
+                    Console.WriteLine("SE (Sound Efffects) folder found");
+                    Shortcutvalue = 3;
+                }
+
+                _Voice:
+                if (IsVoice(TestMatch) == true)
+                {
+                    Console.WriteLine("Voice folder found");
+                    Shortcutvalue = 4;
+                }
+
+                _Default:
+                if (IsDefault(TestMatch) == true)
+                {
+                    Console.WriteLine("Default folder found");
+                }
+
+            }
+
+            //-------------------------------------------------------------------------------//
+            //                             Sound Def Folder                                  //
+            //-------------------------------------------------------------------------------//
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var SounddefFolderAll = ParentFile.Descendants("sounddeffolder");
+            Console.WriteLine("Checking Sound Def Folder");
             foreach (var ChildElement in SounddefFolderAll)
             {
-                Console.WriteLine("Chicking if this element belongs to current Bone...");
-                Console.WriteLine();
-                Console.WriteLine(ChildElement.Descendants("name").FirstOrDefault().Value);
+                Console.ForegroundColor = ConsoleColor.White;
                 var TestBelonging = ChildElement.Descendants("name").FirstOrDefault().Value;
                 IsChildValid(TestBelonging, BoneName);
                 if (IsChildValid(TestBelonging, BoneName) == true)
                 {
-                    Console.WriteLine("SoundBone" + BoneName + " :content has been found");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("SoundBone " + BoneName + ": Sound Def content has been found!");
+                    Console.WriteLine();
+                    RunGenerator(ChildElement, "sounddeffolder", BoneName);
                     break;
                 }
             }
+
+            //-------------------------------------------------------------------------------//
+            //                             Event Group                                       //
+            //-------------------------------------------------------------------------------//
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var EventGroupFolderAll = ParentFile.Descendants("soundbank");
+            Console.WriteLine("Checking Event Group");
+            foreach (var ChildElement in EventGroupFolderAll)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                var TestBelonging = ChildElement.Descendants("name").FirstOrDefault().Value;
+                IsChildValid(TestBelonging, BoneName);
+                if (IsChildValid(TestBelonging, BoneName) == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("SoundBone " + BoneName + " : Event Group content has been found");
+                    Console.WriteLine();
+                    break;
+                }
+            }
+            //-------------------------------------------------------------------------------//
+            //                             Sound Banks                                       //
+            //-------------------------------------------------------------------------------//
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var SoundBankFolderAll = ParentFile.Descendants("soundbank");
+            Console.WriteLine("Checking Soundbanks");
+            foreach (var ChildElement in SoundBankFolderAll)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                var TestBelonging = ChildElement.Descendants("name").FirstOrDefault().Value;
+                IsChildValid(TestBelonging, BoneName);
+                if (IsChildValid(TestBelonging, BoneName) == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("SoundBone " + BoneName + " : SoundBanks content has been found");
+                    break;
+                }
+            }
+
 
             //-------------------------------------------------------------------------------
             // LOAD ALL CHILDREN NAMES
@@ -83,6 +177,8 @@ namespace BoneSync_02
             }
 
         }
+
+        // BOOLEANS DETERMINE IF THE FOLDER I'M IN IS SPECIFIC AND IF IT BELONGS TO THE CHILD I'M GENERATING
 
         public static bool IsChildValid(string TestedElement, string BoneName)
         {
@@ -115,5 +211,86 @@ namespace BoneSync_02
                 return false;
             }
         }
+
+        public static bool IsGhost(string TestedElement)
+        {
+            diff_match_patch MatchTest = new diff_match_patch();
+            MatchTest.Match_Threshold = 0.03f;
+            MatchTest.Match_Distance = 1;
+            var Match = MatchTest.match_main(TestedElement, "Ghost", 0);
+            if (Match == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsSE(string TestedElement)
+        {
+            diff_match_patch MatchTest = new diff_match_patch();
+            MatchTest.Match_Threshold = 0.03f;
+            MatchTest.Match_Distance = 1;
+            var Match = MatchTest.match_main(TestedElement, "SE", 0);
+            if (Match == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsVoice(string TestedElement)
+        {
+            diff_match_patch MatchTest = new diff_match_patch();
+            MatchTest.Match_Threshold = 0.03f;
+            MatchTest.Match_Distance = 1;
+            var Match = MatchTest.match_main(TestedElement, "Voice", 0);
+            if (Match == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsDefault(string TestedElement)
+        {
+            diff_match_patch MatchTest = new diff_match_patch();
+            MatchTest.Match_Threshold = 0.03f;
+            MatchTest.Match_Distance = 1;
+            var Match = MatchTest.match_main(TestedElement, "Default", 0);
+            if (Match == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // CHILD WRITER - Code Responsible for regenerating the soundbones
+
+        public static void RunGenerator(XElement ParentTargetContent, string ReplacementTarget, string BoneName)
+
+            //                                  !!!NEEDS TESTING!!!
+
+        {
+            // select node from one doc
+            XDocument SoundBone = XDocument.Load(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\V2.0\TestFiles\XML\"+BoneName+".xml");
+            XElement ToReplace = SoundBone.Descendants(ReplacementTarget).First();
+
+            // replace one xml node with another
+            ToReplace.ReplaceWith(ParentTargetContent);
+            SoundBone.Save(@"C:\Users\lvalsassina\Documents\GitHub\BoneSync\BoneSync\V2.0\TestFiles\XML\" + BoneName + "Generated.xml");
+        }
+
     }
 }
---
+++ BoneSync/V2.0/BoneSync_02/BoneSync_02/XmlToFdp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Drawing;
+using System.Data;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace BoneSync_02
+{
+    class XmlToFdp
+    {
+        public static void Execute()
+        //Copies over the GENRATED CHILDREN projects as FDP
+        {
+            var SoundSoulsFDP = Directory.GetFiles(@"G:\BoneSync\BoneSync\BoneSync\V2.0\TestFiles\FDP", "Parent.fdp", SearchOption.TopDirectoryOnly);
+            File.Move(@"G:\BoneSync\BoneSync\BoneSync\V2.0\TestFiles\XML\Generated\Child1.xml", @"G:\BoneSync\BoneSync\BoneSync\V2.0\TestFiles\FDP\Child1.xml");
+            File.Move(@"G:\BoneSync\BoneSync\BoneSync\V2.0\TestFiles\FDP\Child1.xml", @"G:\BoneSync\BoneSync\BoneSync\V2.0\TestFiles\XML\Child1.fdp");
+        }
+    }
+}
